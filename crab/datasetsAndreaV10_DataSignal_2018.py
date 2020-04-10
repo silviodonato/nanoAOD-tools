82584982baf2c4c8f55a83661ccc736784097af6
silviodonato <silvio.donato@cern.ch>
2020-04-10 10:12:36 +0200

    fix ZH and ttH sample, add EWK samples

--- crab/datasetsAndreaV10_DataSignal_2018.py
+++ crab/datasetsAndreaV10_DataSignal_2018.py
@@ -31,7 +31,7 @@ data2018 = {
 mc2018 = {
 ### Signal 125 ###
 "ggHmm_2018AMCPY"       : [
-#    '/GluGluHToMuMu_M125_TuneCP5_PSweights_13TeV_amcatnloFXFX_pythia8/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2018_NANOV8a_018_realistic_v15-v2-f46cb006ca3922cbc5562884b4809ef9/USER',
+    '/GluGluHToMuMu_M125_TuneCP5_PSweights_13TeV_amcatnloFXFX_pythia8/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_018_realistic_v15-v2-d92a4324cf7915102060a1e7868b1f8f/USER',
 ],
 "ggHmm_2018POWPY"       : [
 #    '/GluGluHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2018_NANOV8a_ealistic_v15_ext1-v1-f46cb006ca3922cbc5562884b4809ef9/USER',
@@ -47,13 +47,13 @@ mc2018 = {
 
 
 "zHmm_2018POWPY"        : [
-#    '/ZH_HToMuMu_ZToAll_M125_TuneCP5_PSweights_13TeV_powheg_pythia8/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2018_NANOV8a_018_realistic_v15-v2-f46cb006ca3922cbc5562884b4809ef9/USER',
-],
-"ttHmm_2018POWPY"       : [
     '/ZH_HToMuMu_ZToAll_M125_TuneCP5_PSweights_13TeV_powheg_pythia8/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_018_realistic_v15-v2-d92a4324cf7915102060a1e7868b1f8f/USER',
 ],
+"ttHmm_2018POWPY"       : [
+    '/ttHToMuMu_M125_TuneCP5_PSweights_13TeV-powheg-pythia8/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_018_realistic_v15-v2-d92a4324cf7915102060a1e7868b1f8f/USER',
+],
 "WplusHmm_2018POWPY"    : [
-#    '/WplusH_HToMuMu_WToAll_M125_TuneCP5_PSweights_13TeV_powheg_pythia8/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2018_NANOV8a_018_realistic_v15-v2-f46cb006ca3922cbc5562884b4809ef9/USER',
+    '/WplusH_HToMuMu_WToAll_M125_TuneCP5_PSweights_13TeV_powheg_pythia8/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_018_realistic_v15-v2-d92a4324cf7915102060a1e7868b1f8f/USER',
 ],
 "WminusHmm_2018POWPY"   : [
     '/WminusH_HToMuMu_WToAll_M125_TuneCP5_PSweights_13TeV_powheg_pythia8/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_018_realistic_v15-v2-d92a4324cf7915102060a1e7868b1f8f/USER',
@@ -62,19 +62,14 @@ mc2018 = {
     '/VBFHToTauTau_M125_13TeV_powheg_pythia8/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_ealistic_v15_ext1-v1-d92a4324cf7915102060a1e7868b1f8f/USER',
 ],
 
-### ACM sample ###
-"EWKZ105FIX2_2018MGHERWIG"     : [
-#    '/EWK_LLJJ_MLL_105-160_ptJ-0_SM_5f_LO_TuneEEC5_13TeV-madgraph-h7/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2018_NANOV8d_410aea6d0b4d52723d06-f46cb006ca3922cbc5562884b4809ef9/USER'
-],
-
 ### EWK low pt samples ###
 
 "EWKZ105CORR_2018MGHERWIG"     : [
-#    '/EWK_LLJJ_MLL_105-160_SM_5f_LO_TuneCH3_13TeV-madgraph-herwig7_corrected/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2018_NANOV8g_018_realistic_v15-v1-91eaff287f592060132a857a833a7efc/USER',
+    '/EWK_LLJJ_MLL_105-160_SM_5f_LO_TuneCH3_13TeV-madgraph-herwig7_corrected/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_018_realistic_v15-v1-d92a4324cf7915102060a1e7868b1f8f/USER',
 ],
 
 "EWKZCORR_2018MGHERWIG"     : [
-#    '/EWK_LLJJ_MLL-50_MJJ-120_TuneCH3_PSweights_13TeV-madgraph-herwig7_corrected/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2018_NANOV8g_018_realistic_v15-v2-91eaff287f592060132a857a833a7efc/USER',
+    '/EWK_LLJJ_MLL-50_MJJ-120_TuneCH3_PSweights_13TeV-madgraph-herwig7_corrected/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_018_realistic_v15-v2-d92a4324cf7915102060a1e7868b1f8f/USER',
 ],
 }
 
