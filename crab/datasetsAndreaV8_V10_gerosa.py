7734f1a3333d3be6f6ab5f2760eb27c6e13c96aa
silviodonato <silvio.donato@cern.ch>
2020-04-14 22:01:18 +0200

    add Gerosa datasets

---
+++ crab/datasetsAndreaV8_V10_gerosa.py
@@ -0,0 +1,37 @@
+data2016 = {
+}
+data2017 = {
+}
+data2018 = {
+}
+
+mc2016 = {
+"vbfHmm_2016POWHERWIG"      : [
+    '/VBFHToMuMu_M-125_TuneCP5_13TeV-powheg-herwigpp/arizzi-RunIISummer16MiniAODv3_FSRNANO2016MCV8h_04392dd37792585c69c0-b5627bf5542cb3e56008c4afca3c6465/USER',
+],
+"vbfHmm_2016POWPYDIPOLE"      : [
+    '/VBFHToMuMu_M-125_TuneCP5_13TeV-powheg-pythia_dipole/arizzi-RunIISummer16MiniAODv3_FSRNANO2016MCV8h_04392dd37792585c69c0-b5627bf5542cb3e56008c4afca3c6465/USER',
+],
+
+}
+
+mc2017 = {
+"vbfHmm_2017POWPYDIPOLE"      : [
+    '/VBFHToMuMu_M-125_TuneCP5_13TeV-powheg-pythia_dipole/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2017_NANOV8h_7a39ab0ed099ff55ceb9-d7d59b1b3918a7b5ede4f0e937af8b3e/USER',
+],
+}
+
+
+mc2018 = {
+"vbfHmm_2018POWHERWIG7"      : [
+    '/VBFHToMuMu_M-125_TuneCP5_13TeV-powheg-herwig7/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_9f12feadf56f2d534f5f-d92a4324cf7915102060a1e7868b1f8f/USER',
+],
+"vbfHmm_2018POWPYDIPOLE"      : [
+    '/VBFHToMuMu_M-125_TuneCP5_13TeV-powheg-pythia_dipole_fixed/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_9f12feadf56f2d534f5f-d92a4324cf7915102060a1e7868b1f8f/USER',
+],
+"ggHmm_2018POWPY"       : [
+    '/GluGluHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_018_realistic_v15-v2-d92a4324cf7915102060a1e7868b1f8f/USER',
+],
+}
+
+
