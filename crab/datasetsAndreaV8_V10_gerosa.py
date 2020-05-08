004731ef9a39cb8e0df5c44ec04d8599b882416b
silviodonato <silvio.donato@cern.ch>
2020-05-08 09:54:45 +0200

    add EWKZ105 dipole

--- crab/datasetsAndreaV8_V10_gerosa.py
+++ crab/datasetsAndreaV8_V10_gerosa.py
@@ -13,11 +13,21 @@ mc2016 = {
     '/VBFHToMuMu_M-125_TuneCP5_13TeV-powheg-pythia_dipole/arizzi-RunIISummer16MiniAODv3_FSRNANO2016MCV8h_04392dd37792585c69c0-b5627bf5542cb3e56008c4afca3c6465/USER',
 ],
 
+"EWKZ105_2016MGPY"      : [
+    '/EWK_LLJJ_MLL_105-160_TuneCP5_13TeV-madgraph-pythia_dipole/arizzi-RunIISummer16MiniAODv3_FSRNANO2016MCV8h_04392dd37792585c69c0-b5627bf5542cb3e56008c4afca3c6465/USER',
+],
 }
 
 mc2017 = {
 "vbfHmm_2017POWPYDIPOLE"      : [
     '/VBFHToMuMu_M-125_TuneCP5_13TeV-powheg-pythia_dipole/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2017_NANOV8h_7a39ab0ed099ff55ceb9-d7d59b1b3918a7b5ede4f0e937af8b3e/USER',
+],
+"vbfHmm_2017POWHERWIG7"      : [
+    '/VBFHToMuMu_M-125_TuneCP5_13TeV-powheg-herwig7_fixed/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2017_NANOV8h_74883e19626ddbf3d288-d7d59b1b3918a7b5ede4f0e937af8b3e/USER',
+],
+
+"EWKZ105_2017MGPY"      : [
+    '/EWK_LLJJ_MLL_105-160_TuneCP5_13TeV-madgraph-pythia_dipole/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2017_NANOV8h_74883e19626ddbf3d288-d7d59b1b3918a7b5ede4f0e937af8b3e/USER',
 ],
 }
 
@@ -32,6 +42,31 @@ mc2018 = {
 "ggHmm_2018POWPY"       : [
     '/GluGluHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8/arizzi-RunIISummer16MiniAODv3_FSRnano18MC_NANOV10b_018_realistic_v15-v2-d92a4324cf7915102060a1e7868b1f8f/USER',
 ],
+
+"EWKZ105_2018MGPY"      : [
+    '/EWK_LLJJ_MLL_105-160_TuneCP5_13TeV-madgraph-pythia_dipole/arizzi-RunIISummer16MiniAODv3_FSRmyNanoProdMc2018_NANOV8h_9f12feadf56f2d534f5f-91eaff287f592060132a857a833a7efc/USER',
+],
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
